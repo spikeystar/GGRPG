955694b7918c283f331e91d9070ff7d1f8914696
spikeystar <johnpault1234@yahoo.com>
2024-04-24 12:13:08 -0700

    PresentBase Testing
    Collisions do not seem to work when animated

--- Scripts/Objects/Collision/Present.gd
+++ Scripts/Objects/Collision/Present.gd
@@ -3,5 +3,5 @@ extends Node2D
 func _ready():
 	$AnimationPlayer.play("Idle")
 	
-func _on_Area2D_area_entered(area):
-	$AnimationPlayer.play("Done")
+#func _on_Area2D_area_entered(area):
+	#$AnimationPlayer.play("Done")
