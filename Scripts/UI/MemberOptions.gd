105b7d9bc93f18391b4687b805745dc55851d857
spikeystar <johnpault1234@yahoo.com>
2024-05-25 01:05:16 -0700

    Visual Bug

--- Scripts/UI/MemberOptions.gd
+++ Scripts/UI/MemberOptions.gd
@@ -4,6 +4,9 @@ extends Sprite
 func _on_Members_member_options():
 	self.show()
 	$VBoxOptions/MemberOptionsCursor.show()
+	$VBoxOptions/MemberOptionsCursor.modulate.a = 0
+	yield(get_tree().create_timer(0.01), "timeout")
+	$VBoxOptions/MemberOptionsCursor.modulate.a = 1
 	yield(get_tree().create_timer(0.2), "timeout")
 	$VBoxOptions/MemberOptionsCursor.member_options = true
 
