4d5549330b6dd9fc0b366c00b0672fb5c5032025
spikeystar <johnpault1234@yahoo.com>
2024-06-14 12:08:15 -0700

    Fixed Doorway Trigger

--- Scripts/Objects/SceneTransition/Exit.gd
+++ Scripts/Objects/SceneTransition/Exit.gd
@@ -3,6 +3,7 @@
 
 extends Area2D
 var entered = false
+var transitioning = false
 
 const TransitionPlayer = preload("res://Objects/SceneTransition/TransitionPlayer.tscn")
 
@@ -18,12 +19,15 @@ export(TransitionType) var transition_type = TransitionType.FADE_TO_BLACK;
 export var height = 0.0
 
 func _ready():
+	yield(get_tree().create_timer(0.5), "timeout")
 	connect("body_entered", self, "_on_body_entered")
 	position.y += height
 
 func _on_body_entered(body):
-	if "is_player_motion_root" in body and body.is_player_motion_root:
+	if "is_player_motion_root" in body and body.is_player_motion_root and not transitioning:
+		transitioning = true
 		_on_touch_area()
+		
 	
 func _on_touch_area():
 	disconnect("body_entered", self, "_on_body_entered")
@@ -32,6 +36,8 @@ func _on_touch_area():
 	var transition = TransitionPlayer.instance()
 	get_tree().get_root().add_child(transition)
 	transition.transition_in(target_scene, _get_animation_name())
+	yield(get_tree().create_timer(2), "timeout")
+	transitioning = false
 	
 	
 	
