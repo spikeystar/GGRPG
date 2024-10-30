fa14c2af2c8bac306fe53881514e37d43fa3c038
spikeystar <johnpault1234@yahoo.com>
2024-10-30 13:51:48 -0700

    Trailer Text

---
+++ Scripts/UI/Trailer_Text.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+const TransitionPlayer = preload("res://UI/BattleTransition.tscn")
+
+func _ready():
+	SceneManager.loading = true
+	PlayerManager.hide_player()
+	
+	yield(get_tree().create_timer(1.5), "timeout")
+	$AnimationPlayer.play("intro")
+	yield(get_tree().create_timer(2.6), "timeout")
+	
+	var transition = TransitionPlayer.instance()
+	add_child(transition)
+	transition.backwards_star()
+	yield(get_tree().create_timer(0.9), "timeout")
+	transition.queue_free()
+
