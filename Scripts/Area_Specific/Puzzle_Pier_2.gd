f2942b52aa43db0bc631adea0cd16c5f43ba91d3
spikeystar <johnpault1234@yahoo.com>
2025-03-21 14:30:02 -0700

    Puzzle Pier 2

---
+++ Scripts/Area_Specific/Puzzle_Pier_2.gd
@@ -0,0 +1,34 @@
+extends Node
+
+func _ready():
+	randomize()
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var cycle = rng.randi_range(1, 100)
+	if cycle <= 50:
+		night()
+	else:
+		day()
+	
+	#EventManager.Pivot_Town = true
+	
+	SceneManager.SceneEnemies = []
+	
+	#SceneManager.location = "Pivot Town"
+#	if Music.id != "Pivot_Town" or not Music.is_playing:
+#		Music.switch_songs()
+#		Music.id = "Pivot_Town"
+#		Music.music()
+
+func day():
+	print("day")
+	$YSort/Background/NightBckgd.hide()
+	
+	$YSort/MiddleGround/Streetlamps/Streetlamp_A/AnimationPlayer.play("Day")
+	
+func night():
+	$YSort/NightRect.show()
+	$YSort/Background/NightBckgd.show()
+	
+	$YSort/MiddleGround/Streetlamps/Streetlamp_A/AnimationPlayer.play("Night")
+
