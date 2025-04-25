f1b6c196cd44b92b3392ebeaf25cb98301c1fef1
spikeystar <johnpault1234@yahoo.com>
2025-04-24 23:41:13 -0700

    Polish
    Intro scene and event triggering polish

--- Scripts/Area_Event.gd
+++ Scripts/Area_Event.gd
@@ -5,7 +5,7 @@ export var height = 0.0
 signal area_event
 var used = false
 var gary = null
-#var activated = false
+var activated = false
 export var cutscene = false
 
 func _ready():
@@ -13,6 +13,7 @@ func _ready():
 	position.y += height
 	yield(get_tree().create_timer(0.3), "timeout")
 	connect("body_entered", self, "_on_body_entered")
+	connect("body_exited", self, "_on_body_exited")
 	
 func _process(delta):
 	gary = PlayerManager.player_motion_root
@@ -21,11 +22,23 @@ func _process(delta):
 func _on_body_entered(body):
 	if "is_player_motion_root" in body and body.is_player_motion_root and not used and gary.pos_z == height:
 		used = true
-		#activated = true
 		area_event()
 		
+	if "is_player_motion_root" in body and body.is_player_motion_root and not used:
+		activated = true
+		
+		
+func _on_body_exited(body):
+	if "is_player_motion_root" in body and body.is_player_motion_root and activated:
+		activated = false
+		
+func _input(event):
+	if Input.is_action_just_pressed("ui_select") and activated and gary.pos_z == height:
+		used = true
+		area_event()
 		
 func area_event():
+	activated = false
 	if not Global.Collected.has(global_position):
 		if not cutscene:
 			SE.effect("Select")
--- Scripts/Players/PartyStats.gd
+++ Scripts/Players/PartyStats.gd
@@ -48,7 +48,7 @@ var damien_trinket : String = "-"
 var party_sp = 20
 var party_max_sp = 20
 var party_level = 1
-var party_exp = 400
+var party_exp = 0
 var next_level = int((100) * (party_level/1.5) + (party_level * 150))
 
 var new_spell_2 = false
