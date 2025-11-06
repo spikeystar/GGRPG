e5b04f5a099887844e53fd573b4b060ddda5bcbd
spikeystar <johnpault1234@yahoo.com>
2025-11-05 23:32:58 -0800

    Battle Trigger

--- Scripts/Objects/EnemyTest.gd
+++ Scripts/Objects/EnemyTest.gd
@@ -94,7 +94,7 @@ func _physics_process(delta):
 	#if spawn_z > 0 and spawn_z < 45 and ground_enemy:
 	#	$MotionRoot/BattleTrigger.global_position = motion_root.global_position + Vector2(0.0, -draw_pos_z + 25)
 	if spawn_z > 0 and spawn_z < 150 and ground_enemy:
-		$MotionRoot/BattleTrigger.global_position = motion_root.global_position + Vector2(0.0, -draw_pos_z + (spawn_z / 1.5))
+		$MotionRoot/BattleTrigger.global_position = motion_root.global_position + Vector2(0.0, -draw_pos_z + (spawn_z / 1.25))
 	
 	if Global.battle_ended:
 		#after_battle()
