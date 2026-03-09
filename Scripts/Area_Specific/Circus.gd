2a95b72f9467e4f31b468736d1105caf2e853588
spikeystar <johnpault1234@yahoo.com>
2026-03-09 01:02:32 -0700

    Enemies

--- Scripts/Area_Specific/Circus.gd
+++ Scripts/Area_Specific/Circus.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 func _ready():
 	SceneManager.location = "Circus"
-	if Music.id != "Circus":
+	
+#	if Music.id != "Circus":
+#		Music.switch_songs()
+#		Music.id = "Circus"
+#		Music.music()
+		
+	if Music.id != "Puzzle_Pier":
 		Music.switch_songs()
-		Music.id = "Circus"
+		Music.id = "Puzzle_Pier"
 		Music.music()
 
 	SceneManager.SceneEnemies = []
