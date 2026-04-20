7fc3e14e135ed25dd1070eb6ed41edc11c2c5baf
spikeystar <johnpault1234@yahoo.com>
2026-04-20 12:16:13 -0700

    Slot Machine

--- Scripts/Area_Specific/Minigame/Slot_Machine.gd
+++ Scripts/Area_Specific/Minigame/Slot_Machine.gd
@@ -14,8 +14,12 @@ func _ready():
 	$Slots/Slot1.frame = 0
 	$Slots/Slot2.frame = 5
 	$Slots/Slot3.frame = 2
+	$Slots/Slot1.speed_scale = 1.2
+	$Slots/Slot2.speed_scale = 1.2
+	$Slots/Slot3.speed_scale = 1.2
 	SE.effect("Menu Open")
 	$AnimationPlayer.play("open")
+	$AnimationPlayer3.play("open")
 	yield(get_tree().create_timer(0.8), "timeout")
 	wagering = true
 	
@@ -36,12 +40,20 @@ static func thousands_sep(number, prefix=''):
 	return res
 
 func _input(event):
+	if Input.is_action_just_pressed("ui_cancel") and wagering or Input.is_action_just_pressed("ui_accept") and wagering:
+		done = false
+		SE.effect("Menu Open")
+		$AnimationPlayer.play_backwards("open")
+		$AnimationPlayer.playback_speed = 1
+		yield(get_tree().create_timer(0.9), "timeout")
+		self.queue_free()
+	
 	if Input.is_action_just_pressed("ui_up") and wagering and current_wager < 10000 and not cooldown:
 		cooldown = true
 		SE.effect("Marble")
 		current_wager += 1000
 		$Title.text = "Wager: " + str(thousands_sep(current_wager))
-		yield(get_tree().create_timer(0.2), "timeout")
+		yield(get_tree().create_timer(0.3), "timeout")
 		cooldown = false
 		
 	if Input.is_action_just_pressed("ui_down") and wagering and current_wager > 1000 and not cooldown:
@@ -49,19 +61,19 @@ func _input(event):
 		SE.effect("Marble")
 		current_wager -= 1000
 		$Title.text = "Wager: " + str(thousands_sep(current_wager))
-		yield(get_tree().create_timer(0.2), "timeout")
+		yield(get_tree().create_timer(0.3), "timeout")
 		cooldown = false
 		
 	if Input.is_action_just_pressed("ui_up") and wagering and current_wager == 10000 and not cooldown:
 		cooldown = true
 		SE.effect("Unable")
-		yield(get_tree().create_timer(0.2), "timeout")
+		yield(get_tree().create_timer(0.3), "timeout")
 		cooldown = false
 		
 	if Input.is_action_just_pressed("ui_down") and wagering and current_wager == 1000 and not cooldown:
 		cooldown = true
 		SE.effect("Unable")
-		yield(get_tree().create_timer(0.2), "timeout")
+		yield(get_tree().create_timer(0.3), "timeout")
 		cooldown = false
 		
 	if Input.is_action_just_pressed("ui_select") and wagering and Party.marbles < current_wager:
@@ -70,6 +82,7 @@ func _input(event):
 	if Input.is_action_just_pressed("ui_select") and wagering and Party.marbles >= current_wager:
 		wagering = false
 		SE.effect("Switch")
+		$AnimationPlayer3.play("RESET")
 		yield(get_tree().create_timer(0.5), "timeout")
 		SE.effect("Metal Door")
 		able = true
@@ -114,6 +127,8 @@ func _input(event):
 		yield(get_tree().create_timer(0.9), "timeout")
 		self.queue_free()
 		
+
+		
 		
 func results():
 	var SlotArray = $Slots.get_children()
@@ -121,6 +136,9 @@ func results():
 	var diamond_count = 0
 	var clover_count = 0
 	var star_count = 0
+	var win
+	var draw
+	var fail
 	for x in SlotArray:
 		if x.frame == 0 or x.frame == 4:
 			spade_count +=1
@@ -134,31 +152,43 @@ func results():
 	if spade_count == 3:
 		Party.marbles += (current_wager * 2)
 		$Notify/Instruction.text = "2X Win!"
-		SE.effect("Win")
+		win = true
 		
 	if diamond_count == 3:
 		Party.marbles += (current_wager * 3)
 		$Notify/Instruction.text = "3X Win!"
-		SE.effect("Win")
+		win = true
 		
 	if clover_count == 3:
 		Party.marbles += (current_wager * 4)
 		$Notify/Instruction.text = "4X Win!"
-		SE.effect("Win")
+		win = true
 		
 	if star_count == 3:
 		Party.marbles += (current_wager * 5)
 		$Notify/Instruction.text = "5X Win!"
-		SE.effect("Win")
+		win = true
 		
-	if spade_count <3 and diamond_count<3 and clover_count<3 and star_count<3:
+	if spade_count ==2 or diamond_count==2 or clover_count==2 or star_count==2:
+		$Notify/Instruction.text = "Draw!"
+		draw = true
+		
+	if spade_count <2 and diamond_count<2 and clover_count<2 and star_count<2:
 		$Notify/Instruction.text = "Try Again!"
-		SE.effect("Fail")
+		Party.marbles -= (current_wager)
+		fail = true
 		
 		
-	yield(get_tree().create_timer(0.5), "timeout")	
+	yield(get_tree().create_timer(0.3), "timeout")	
 	$Notify/AnimationPlayer.play("open")
+	$Notify/AnimationPlayer2.play("flicker")
 	$Notify.show()
+	if win:
+		SE.effect("Win")
+	if draw:
+		SE.effect("Select")
+	if fail:
+		SE.effect("Fail")
 	
 	yield(get_tree().create_timer(0.5), "timeout")
 	done = true
